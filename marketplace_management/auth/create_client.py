a811196bc91a801cad8a5b1211510efaeeae0078
Nabeel Goraya <itsgoraya@gmail.com>
2024-02-29 09:00:40 +0500

    update

---
+++ marketplace_management/auth/create_client.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2023, Zaviago and contributors
+# For license information, please see license.txt
+
+import base64
+from typing import Any, Dict, List, Optional, Tuple
+from urllib.parse import urlencode
+import frappe
+import requests
+from frappe import _
+from frappe.utils import cint, cstr, get_datetime
+from pytz import timezone
+import random
+import webbrowser
+import json
+import hmac
+import hashlib
+import time
+
+
+class CreateMarketplaceClient:
+
+    def start_connecting_shopee( self, app_key , app_secret,client_site):
+        return self.createAuthRequest_shopee(app_key,app_secret,client_site)
+    
+    def start_connecting_lazada( self, app_key , app_secret,client_site):
+            return self.createAuthRequest_lazada(app_key,app_secret,client_site)
+
+    def createAuthRequest_lazada( self, app_key , app_secret,client_site):
+        endpoint = "/api/method/marketplace_integration.auth.create_client.receive_code_from_lazada"
+        return_url = client_site
+        host = "https://auth.lazada.com"
+        path = "/oauth/authorize"
+        redirect_url = return_url+endpoint
+        partner_id = app_key
+        url = host + path + "?response_type=code&force_auth=true&redirect_uri=%s&client_id=%s" % (redirect_url,partner_id)
+        return url
+        
+    def createAuthRequest_shopee( self, app_key , app_secret,client_site):
+        endpoint = "/api/method/marketplace_integration.auth.create_client.receive_code_from_shopee"
+        return_url = client_site
+        timest = int(time.time())
+        host = "https://partner.shopeemobile.com"
+        path = "/api/v2/shop/auth_partner"
+        redirect_url = return_url+endpoint
+        partner_id = app_key
+        tmp = app_secret
+        partner_key = tmp.encode()
+        tmp_base_string = "%s%s%s" % (partner_id, path, timest)
+        base_string = tmp_base_string.encode()
+        sign = hmac.new(partner_key, base_string, hashlib.sha256).hexdigest()
+        ##generate api
+        url = host + path + "?partner_id=%s&timestamp=%s&sign=%s&redirect=%s" % (partner_id, timest, sign, redirect_url)
+        return url
+
+@frappe.whitelist(allow_guest=True)
+def redirect_to_auth_shopee(client_site):
+    app_details = frappe.get_doc('Marketplace Management') 
+    if( app_details.partner_id!='' and app_details.partner_key != '' and app_details.active_shopee ):
+            connect = CreateMarketplaceClient()	
+            url = connect.start_connecting_shopee ( app_details.partner_id,app_details.partner_key,client_site)
+            if( url ):
+                print (f"Redirecting to auth from function {url}")
+                response_ = {'url':url}
+                return response_
+                exit() 	
+    return   
+
+@frappe.whitelist(allow_guest=True)
+def redirect_to_auth_lazada(client_site):
+    app_details = frappe.get_doc('Marketplace Management') 
+    if( app_details.client_id !='' and app_details.client_secret != '' and app_details.active_lazada ):
+            connect = CreateMarketplaceClient()	
+            url = connect.start_connecting_lazada ( app_details.client_id,app_details.client_secret,client_site)
+            if( url ):
+                print (f"Redirecting to auth from function {url}")
+                response_ = {'url':url}
+                return response_
+                exit() 	
+    return 
+
+@frappe.whitelist(allow_guest=True)
+def code_to_token_auth_shopee(code,shop_id):
+    
+    if(code !='' and shop_id != ''):
+        app_details = frappe.get_doc('Marketplace Management') 
+        return get_token_shop_level(code,  app_details.partner_id, app_details.partner_key, shop_id)
+
+def get_token_shop_level(code, partner_id, tmp_partner_key, shop_id):
+    partner_id = int(partner_id)
+    shop_id = int(shop_id)
+    timest = int(time.time())
+    host = "https://partner.shopeemobile.com"
+    path = "/api/v2/auth/token/get"
+    body = {"code": code, "shop_id": shop_id, "partner_id": partner_id}
+    tmp_base_string = "%s%s%s" % (partner_id, path, timest)
+    base_string = tmp_base_string.encode()
+    partner_key = tmp_partner_key.encode()
+    sign = hmac.new(partner_key, base_string, hashlib.sha256).hexdigest()
+    url = host + path + "?partner_id=%s&timestamp=%s&sign=%s" % (partner_id, timest, sign)
+    # print(url)
+    headers = {"Content-Type": "application/json"}
+    resp = requests.post(url, json=body, headers=headers)
+    ret = json.loads(resp.content)
+    access_token = ret.get("access_token")
+    new_refresh_token = ret.get("refresh_token")
+    return {"access_token": access_token, "new_refresh_token": new_refresh_token}
---
+++ marketplace_management/marketplace_management/doctype/marketplace_management/marketplace_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, zaviago and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Marketplace Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
